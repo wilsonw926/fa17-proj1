1e1ecc4ca5ae2f2c53b21b9b4f313c18794e8c6f
Wilson Wang <wilsonw926@gmail.com>
2017-10-21 16:29:16 -0700

    damage pokemon

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,5 @@ Rails.application.routes.draw do
   root to: "home#index"
   resources :trainers
   patch '/capture', to: 'pokemons#capture', as: 'capture'
+  patch '/damage', to: 'pokemons#damage', as: 'damage'
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171021181516) do
+ActiveRecord::Schema.define(version: 20171021225800) do
 
   create_table "pokemons", force: :cascade do |t|
     t.string "name"
@@ -18,6 +18,7 @@ ActiveRecord::Schema.define(version: 20171021181516) do
     t.integer "trainer_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "health"
   end
 
   create_table "trainers", force: :cascade do |t|
