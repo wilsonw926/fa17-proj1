c90a945f26cba3188bc5e3ed31c4957e54930cde
Wilson Wang <wilsonw926@gmail.com>
2017-10-21 14:37:49 -0700

    finished part 1

--- app/models/trainer.rb
+++ app/models/trainer.rb
@@ -1,4 +1,5 @@
 class Trainer < ApplicationRecord
+  has_many :pokemons
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
