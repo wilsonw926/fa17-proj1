40cc03f3c0dba8351e253d9abc032ecf980305a3
Wilson Wang <wilsonw926@gmail.com>
2017-10-21 17:58:00 -0700

    finished proj

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,6 @@ Rails.application.routes.draw do
   resources :trainers
   patch '/capture', to: 'pokemons#capture', as: 'capture'
   patch '/damage', to: 'pokemons#damage', as: 'damage'
+  get 'pokemons/new', to: "pokemons#new", as: 'new_pokemon'
+  post 'pokemons/new', to: "pokemons#create"
 end
